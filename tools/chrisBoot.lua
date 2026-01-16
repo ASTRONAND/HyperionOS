55fdddeff8ee02afcf0be542ed636aa3b76d3891
Astronand <astronandyt@gmail.com>
2026-01-16 16:55:41 -0500

    hotfix

--- tools/chrisBoot.lua
+++ tools/chrisBoot.lua
@@ -1,3 +1,6 @@
+local diskpath="put your path here"
+periphemu.create("right", "drive")
+disk.insertDisk("right", diskpath)
 local file = fs.open("/disk/boot/cct/eeprom")
 local text = file.readAll()
 file.close()
