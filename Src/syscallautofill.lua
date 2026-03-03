b7f52dd17bdfee6a69801f7030d104ad6f5b03b2
Astronand <astronandyt@gmail.com>
2026-03-02 22:25:37 -0500

    working on syscall manifest and fixed anther exploit

---
+++ Src/syscallautofill.lua
@@ -0,0 +1,101 @@
+syscall={}
+syscall.sethomedir=function(uid, homedir) end
+syscall.read=function(fd, amount) end
+syscall.getTask=function(pid) end
+syscall.connect=function(fd, address) end
+syscall.getcwd=function() end
+syscall.lodetach=function(id) end
+syscall.stop=function(pid) end
+syscall.recv
+syscall.write=function(fd, data) end
+syscall.getppid=function() end
+syscall.lstat
+syscall.open=function(path, mode) end
+syscall.lseek=function(fd, offset, whence) end
+syscall.setHostname=function(hostname) end
+syscall.chroot
+syscall.dup2
+syscall.getpid=function() end
+syscall.fchown
+syscall.close=function(fd) end
+syscall.umount
+syscall.getTasks=function() end
+syscall.sysdump=function() end
+syscall.fchmod
+syscall.getHostname=function() end
+syscall.listen
+syscall.dup
+syscall.gpio_read
+syscall.fget_suid
+syscall.gpio_write
+syscall.setpassword
+syscall.setEnviron=function(key, value) end
+syscall.losetup
+syscall.reboot=function() end
+syscall.getuid=function() end
+syscall.sigsend
+syscall.sleep=function(time) end
+syscall.exit=function(code) end
+syscall.getEnviron=function(key) end
+syscall.continue=function(pid) end
+syscall.socket
+syscall.log=function(text, tag, color) end
+syscall.loimgwrite
+syscall.exists=function(path) end
+syscall.setuid=function(uid) end
+syscall.exec=function(path, args, envars) end
+syscall.execspawn=function(path, name, envars, args, tgid) end
+syscall.loimgcreate
+syscall.time=function() end
+syscall.newuser
+syscall.spawn=function(func, name, envars, args, tgid) end
+syscall.collect=function(pid) end
+syscall.setshell
+syscall.devctl=function(fd, funcname) end
+syscall.listusers
+syscall.unlockuser
+syscall.mount
+syscall.accept
+syscall.lolist
+syscall.readlink
+syscall.deleteuser
+syscall.remove=function(path) end
+syscall.type=function(path) end
+syscall.elevate
+syscall.mkdir=function(path) end
+syscall.getuidbyname
+syscall.whoami=function() end
+syscall.sendfile
+syscall.setusername
+syscall.geteuid
+syscall.login
+syscall.getHost
+syscall.getUptime
+syscall.httpget
+syscall.stat
+syscall.symlink
+syscall.pread
+syscall.chdir
+syscall.arch=function() end
+syscall.pwrite
+syscall.sockshutdown
+syscall.resolve
+syscall.send
+syscall.fstat
+syscall.chown
+syscall.fsync
+syscall.lockuser
+syscall.getUsername
+syscall.getsockname
+syscall.bind
+syscall.kill=function(pid) end
+syscall.setgid
+syscall.getpeername
+syscall.sigcatch
+syscall.shutdown
+syscall.access
+syscall.sigignore
+syscall.getpasswd
+syscall.version
+syscall.chmod=function(path, perms) end
+syscall.listdir=function(path) end
